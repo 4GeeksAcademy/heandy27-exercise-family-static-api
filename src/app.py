3ecdec9076265b9eb22f953f544c5d44798796b8
Heandy27 <122308561+Heandy27@users.noreply.github.com>
2024-05-14 22:11:40 +0000

    [Creacion de funciones] [getAllMembers, getMember, deleteMember]

--- src/app.py
+++ src/app.py
@@ -30,13 +30,47 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
+    response_body = members
+    return jsonify(response_body), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    print("position ejemplo", id)
+
+    if member is None:
+        return jsonify({"msg": "El miembro del id solicitado no fue encontrado"}), 400
+    return jsonify(member), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def get_delete_member(id):
+    jackson_family.delete_member(id)
+    return jsonify({"done": True}), 200
+
+@app.route('/member', methods=['POST'])
+def add_new_member():
+    body = request.get_json(silent=True)
+    if body is None:
+        return jsonify({"mgs": "Debes enviar informacion en el body"}), 400
+    if "first_name" not in body:
+        return jsonify({"mgs": "Debes enviar informacion en el first_name"}), 400
+    
+    id = jackson_family._generateId()
+
+    if "id" in body:
+        id = body["id"]
+
+    new_member_data = {
+        "id": id,
+        "first_name": body["first_name"],
+        "last_name": jackson_family.last_name,
+        "age": body["age"],
+        "lucky_numbers": body["lucky_numbers"]
     }
 
+    jackson_family.add_member(new_member_data)
 
-    return jsonify(response_body), 200
+    return jsonify({"mgs": "Se ha creado un nuevo miembro"}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
